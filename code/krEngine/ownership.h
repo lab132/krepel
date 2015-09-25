70ebf12a3aa403860cca47eebd109b680dffcd02
Manuel Maier <mjmaier@gmx.de>
2015-09-25 12:45:08 +0100

    Put OwnershipData<> in its own header

--- code/krEngine/ownership.h
+++ code/krEngine/ownership.h
@@ -1,4 +1,5 @@
 #pragma once
 
+#include <krEngine/ownership/ownershipData.h>
 #include <krEngine/ownership/owned.h>
 #include <krEngine/ownership/borrowed.h>
--- code/krEngine/ownership/owned.h
+++ code/krEngine/ownership/owned.h
@@ -1,23 +1,10 @@
 #pragma once
 
-#include <Foundation/Threading/AtomicUtils.h>
 #include <krEngine/common/traits.h>
+#include <krEngine/ownership/ownershipData.h>
 
 namespace kr
 {
-  template<typename T>
-  struct OwnershipData
-  {
-    T* ptr = nullptr;
-    mutable ezInt64 refCount = 0;
-  };
-
-  template<typename T>
-  void addRef(const OwnershipData<T>& data) { ezAtomicUtils::Increment(data.refCount); }
-
-  template<typename T>
-  void removeRef(const OwnershipData<T>& data) { ezAtomicUtils::Decrement(data.refCount); }
-
   /// \brief Wrapper for ownership over an instance of an object.
   template<typename T>
   struct Owned
